c1261000d0274b9bec55bd90eab2fa9cd826906e
Marc Delagrammatikas <delagrammatikas@gmail.com>
2016-11-22 13:01:05 -0800

    Refactor to extract methods.

--- content.js
+++ content.js
@@ -1,24 +1,47 @@
-var elements = document.getElementsByTagName('*');
-
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/Alt Right/gi, 'Neo Nazis')
-                                   .replace(/Alt-Right/gi, 'Neo-Nazis')
-                                   .replace(/Alternative Right/gi, 'Neo Nazis')
-                                   .replace(/National Policy Institute/gi, 'Nazi Party')
-                                   .replace(/Breitbart/gi, 'Reich Ministry of Public Enlightenment')
-                                   .replace(/Breitbart News Network/gi, 'Reich Ministry of Public Enlightenment')
-                                   .replace(/Richard Spencer/gi, 'Hipster Hitler');
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-    }
+var isTextNode = function(node) {
+  return node.nodeType === 3;
 }
+
+var getAllDomElements = function() {
+  return document.getElementsByTagName('*');
+}
+
+var processElements = function(elements) {
+  for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+
+      processChildNodes(element);
+  }
+}
+
+var processChildNodes = function(element) {
+  for (var j = 0; j < element.childNodes.length; j++) {
+      var node = element.childNodes[j];
+
+      processNode(element, node);
+  }
+}
+
+var processNode = function(parentElement, node) {
+  if (!isTextNode(node)) { return; }
+
+  var text = node.nodeValue;
+  var replacedText = replaceText(text);
+
+  if (replacedText !== text) {
+      parentElement.replaceChild(document.createTextNode(replacedText), node);
+  }
+}
+
+var replaceText = function(text) {
+  return text.replace(/Alt Right/gi, 'Neo Nazis')
+             .replace(/Alt-Right/gi, 'Neo-Nazis')
+             .replace(/Alternative Right/gi, 'Neo Nazis')
+             .replace(/National Policy Institute/gi, 'Nazi Party')
+             .replace(/Breitbart/gi, 'Reich Ministry of Public Enlightenment')
+             .replace(/Breitbart News Network/gi, 'Reich Ministry of Public Enlightenment')
+             .replace(/Richard Spencer/gi, 'Hipster Hitler');
+}
+
+var elements = getAllDomElements();
+processElements(elements);
